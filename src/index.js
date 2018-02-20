a87d50d9457f080d67605bebaa79c9d26adb2250
Thomas Trinca <tomtrinca@protonmail.com>
2018-02-20 17:58:11 +1100

    refactor: change import to App.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import BinaryClock from './BinaryClock';
+// import BinaryClock from './BinaryClock';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BinaryClock />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
